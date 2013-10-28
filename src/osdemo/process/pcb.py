af156ad660b6a825717638e36062585eafaa6ae3
hslavich <hernan.slavich@gmail.com>
2013-10-28 20:37:48 -0300

    Add pcb status

--- src/osdemo/process/pcb.py
+++ src/osdemo/process/pcb.py
@@ -1,10 +1,18 @@
 class PCB(object):
 
+    NEW = 0
+    READY = 1
+    RUNNING = 2
+    WAITING_IO = 3
+    IO = 4
+    END = 5
+
     def __init__(self, program, pid):
         self.program = program
         self.pid = pid
         self.pc = 0
         self.priority = 0
+        self.state = PCB.NEW
         self.total_instructions = len(program.instructions)
 
     def increment_pc(self):
