4424d413db9736a9e410beb29da5547b9d5d4d66
hslavich <hernan.slavich@gmail.com>
2013-11-11 21:20:27 -0300

    Add command for program loading

--- __main__.py
+++ __main__.py
@@ -2,9 +2,11 @@ import logging, sys
 from osdemo.core.kernel import Kernel
 from osdemo.process.program import Program
 from osdemo.process.instruction import Instruction
+from osdemo.shell.commands import Commands
 
 sys.path.append('lib/pyshell')
 from pyshell.model.shell import Shell
+from pyshell.gui.shellframe import ShellFrame
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s | %(message)s', level=logging.DEBUG)
@@ -19,3 +21,8 @@ if __name__ == '__main__':
     k.load(p)
     k.load(p2)
     k.load(p3)
+
+    shell = Shell()
+    shell.addCommand("load", Commands(k).load)
+    ShellFrame(shell).start()
+
---
+++ osdemo/shell/commands.py
@@ -0,0 +1,36 @@
+import logging
+from osdemo.process.program import Program
+from osdemo.process.instruction import Instruction
+
+class Commands(object):
+
+    def __init__(self, kernel):
+        super(Commands, self).__init__()
+        self.kernel = kernel
+
+    def _parse_instructions(self, name):
+        f = open("programs/%s" % name, 'r')
+        return [l.strip() for l in f.readlines()]
+
+    def _create_program(self, name, instrs):
+        program = Program(name)
+        for instr in instrs:
+            program.instructions.append(Instruction(instr))
+        return program
+
+    def _run(self, name, priority):
+        try:
+            instr = self._parse_instructions(name)
+            prog = self._create_program(name, instr)
+            self.kernel.load(prog, priority)
+        except Exception, e:
+            logging.error(e)
+
+    def load(self, args):
+        try:
+            name = args[0]
+            priority = args[1]
+        except IndexError:
+            priority = None
+
+        self._run(name, priority)
