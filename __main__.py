e287f2f242e829c5b9fd00f31ecaecd32aa326a6
hslavich <hernan.slavich@gmail.com>
2013-11-26 17:52:10 -0300

    Remove process execution from main

--- __main__.py
+++ __main__.py
@@ -1,7 +1,5 @@
 import logging, sys
 from osdemo.core.kernel import Kernel
-from osdemo.process.program import Program
-from osdemo.process.instruction import Instruction
 from osdemo.shell.commands import Commands
 
 sys.path.append('lib/pyshell')
@@ -11,16 +9,6 @@ from pyshell.gui.shellframe import ShellFrame
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s | %(message)s', level=logging.DEBUG)
     k = Kernel()
-
-    p = Program("test")
-    p.instructions = [Instruction("CPU"), Instruction("CPU"), Instruction("CPU")]
-    p2 = Program("test2")
-    p2.instructions = [Instruction("CPU"), Instruction("CPU"), Instruction("CPU")]
-    p3 = Program("test3")
-    p3.instructions = [Instruction("CPU"), Instruction("CPU"), Instruction("CPU")]
-    k.load(p)
-    k.load(p2)
-    k.load(p3)
 
     shell = Shell()
     shell.addCommand("load", Commands(k).load)
