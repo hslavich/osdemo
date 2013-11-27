eb7daa53b2b2b1561c5cf6922e868ed3d1a0e02e
hslavich <hernan.slavich@gmail.com>
2013-11-26 23:13:39 -0300

    Dynamic change algorithm method

--- osdemo/scheduling/scheduler.py
+++ osdemo/scheduling/scheduler.py
@@ -17,11 +17,12 @@ class Scheduler(object):
         return self.strategy.choose_next()
 
     def set_algorithm(self, algo):
-        if algo == "priority":
-            self.strategy = algorithms.Priority(self)
-        elif algo == "FCFS":
-            self.strategy = algorithms.FCFS(self)
-        else:
-            raise Exception("Can't set algorithm %s" % algo)
-        logging.debug("SCHEDULER algorithm set %s" % algo)
+        try:
+            m = __import__("osdemo.scheduling.algorithms")
+            m = getattr(m, "scheduling")
+            m = getattr(m, "algorithms")
+            self.strategy = getattr(m, algo)(self)
+        except Exception:
+            raise Exception("Can't set algorithm " + algo)
+        logging.info("SCHEDULER algorithm set %s" % algo)
 
